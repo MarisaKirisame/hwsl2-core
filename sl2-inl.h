2f45d8edeb64e47d94448fc15553771e4dcd892b
waterlens <waterlens@waterlens.moe>
2025-02-03 22:53:51 +0800

    unaligned L/S

--- sl2-inl.h
+++ sl2-inl.h
@@ -1,29 +1,34 @@
 #pragma once
 
 #include "gf2p127-inl.h"
+#include "sse2neon/sse2neon.h"
 #include <stdio.h>
+#include <string.h>
 
 typedef gf2p127_t sl2_t[2][2];
 typedef gf2p127_t (*psl2_t)[2];
 
+#define sl2_elem_get(x, idx1, idx2) (_mm_loadu_si128(&(x)[(idx1)][(idx2)]))
+#define sl2_elem_set(x, idx1, idx2, v) (_mm_storeu_si128(&(x)[(idx1)][(idx2)], (v)))
+
 static inline
 _Bool sl2_valid(sl2_t a) {
-  gf2p127_t det = gf2p127_add(gf2p127_mul(a[0][0], a[1][1]),
-                              gf2p127_mul(a[0][1], a[1][0]));
+  gf2p127_t det = gf2p127_add(gf2p127_mul(sl2_elem_get(a, 0, 0), sl2_elem_get(a, 1, 1)),
+                              gf2p127_mul(sl2_elem_get(a, 0, 1), sl2_elem_get(a, 1, 0)));
   return _mm_extract_epi64(det, 0) == 1 &&
          _mm_extract_epi64(det, 1) == 0 &&
-         gf2p127_valid(a[0][0]) &&
-         gf2p127_valid(a[0][1]) &&
-         gf2p127_valid(a[1][0]) &&
-         gf2p127_valid(a[1][1]);
+         gf2p127_valid(sl2_elem_get(a, 0, 0)) &&
+         gf2p127_valid(sl2_elem_get(a, 0, 1)) &&
+         gf2p127_valid(sl2_elem_get(a, 1, 0)) &&
+         gf2p127_valid(sl2_elem_get(a, 1, 1));
 }
 
 static inline
 _Bool sl2_eq(sl2_t a, sl2_t b) {
-  return gf2p127_eq(a[0][0], b[0][0]) &&
-         gf2p127_eq(a[0][1], b[0][1]) &&
-         gf2p127_eq(a[1][0], b[1][0]) &&
-         gf2p127_eq(a[1][1], b[1][1]);
+  return gf2p127_eq(sl2_elem_get(a, 0, 0), sl2_elem_get(b, 0, 0)) &&
+         gf2p127_eq(sl2_elem_get(a, 0, 1), sl2_elem_get(b, 0, 1)) &&
+         gf2p127_eq(sl2_elem_get(a, 1, 0), sl2_elem_get(b, 1, 0)) &&
+         gf2p127_eq(sl2_elem_get(a, 1, 1), sl2_elem_get(b, 1, 1));
 }
 
 static inline
@@ -47,10 +52,7 @@ int sl2_cmp(sl2_t a, sl2_t b) {
 
 static inline
 void sl2_copy(sl2_t dst, sl2_t src) {
-  dst[0][0] = src[0][0];
-  dst[0][1] = src[0][1];
-  dst[1][0] = src[1][0];
-  dst[1][1] = src[1][1];
+  memcpy(dst, src, sizeof(sl2_t));
 }
 
 #ifdef __AVX2__
@@ -109,30 +111,30 @@ void sl2_mul_bits_left(gf2p127_t *b00, gf2p127_t *b01, gf2p127_t *b10, gf2p127_t
 #ifdef __AVX2__
 static inline
 void sl2_mul_buf_left(sl2_t b, unsigned char *buf, size_t n) {
-  gf2p127x2_t b00b01 = _mm256_loadu2_m128i(&b[0][0], &b[0][1]);
-  gf2p127x2_t b10b11 = _mm256_loadu2_m128i(&b[1][0], &b[1][1]);
+  gf2p127x2_t b00b01 = _mm256_loadu2_m128i(&sl2_elem_get(b, 0, 0), &sl2_elem_get(b, 0, 1));
+  gf2p127x2_t b10b11 = _mm256_loadu2_m128i(&sl2_elem_get(b, 1, 0), &sl2_elem_get(b, 1, 1));
   size_t i;
   for (i = n; i > 0; i--) {
     sl2_mul_bits_left_x2(&b00b01, &b10b11, buf[i - 1]);
   }
-  _mm256_storeu2_m128i(&b[0][0], &b[0][1], b00b01);
-  _mm256_storeu2_m128i(&b[1][0], &b[1][1], b10b11);
+  _mm256_storeu2_m128i(&sl2_elem_get(b, 0, 0), &sl2_elem_get(b, 0, 1), b00b01);
+  _mm256_storeu2_m128i(&sl2_elem_get(b, 1, 0), &sl2_elem_get(b, 1, 1), b10b11);
 }
 #else
 static inline
 void sl2_mul_buf_left(sl2_t b, unsigned char *buf, size_t n) {
-  gf2p127_t b00 = b[0][0];
-  gf2p127_t b01 = b[0][1];
-  gf2p127_t b10 = b[1][0];
-  gf2p127_t b11 = b[1][1];
+  gf2p127_t b00 = sl2_elem_get(b, 0, 0);
+  gf2p127_t b01 = sl2_elem_get(b, 0, 1);
+  gf2p127_t b10 = sl2_elem_get(b, 1, 0);
+  gf2p127_t b11 = sl2_elem_get(b, 1, 1);
   size_t i;
   for (i = n; i > 0; i--) {
     sl2_mul_bits_left(&b00, &b01, &b10, &b11, buf[i - 1]);
   }
-  b[0][0] = b00;
-  b[0][1] = b01;
-  b[1][0] = b10;
-  b[1][1] = b11;
+  sl2_elem_set(b, 0, 0, b00);
+  sl2_elem_set(b, 0, 1, b01);
+  sl2_elem_set(b, 1, 0, b10);
+  sl2_elem_set(b, 1, 1, b11);
 }
 #endif
 
@@ -193,29 +195,29 @@ void sl2_mul_bits_right(gf2p127_t *a00, gf2p127_t *a01, gf2p127_t *a10, gf2p127_
 static inline
 void sl2_mul_buf_right(sl2_t a, unsigned char *buf, size_t n) {
   size_t i;
-  gf2p127x2_t a00a10 = _mm256_loadu2_m128i(&a[0][0], &a[1][0]);
-  gf2p127x2_t a01a11 = _mm256_loadu2_m128i(&a[0][1], &a[1][1]);
+  gf2p127x2_t a00a10 = _mm256_loadu2_m128i(&sl2_elem_get(a, 0, 0), &sl2_elem_get(a, 1, 0));
+  gf2p127x2_t a01a11 = _mm256_loadu2_m128i(&sl2_elem_get(a, 0, 1), &sl2_elem_get(a, 1, 1));
   for (i = 0; i < n; i++) {
     sl2_mul_bits_right_x2(&a00a10, &a01a11, buf[i]);
   }
-  _mm256_storeu2_m128i(&a[0][1], &a[1][1], a01a11);
-  _mm256_storeu2_m128i(&a[0][0], &a[1][0], a00a10);
+  _mm256_storeu2_m128i(&sl2_elem_get(a, 0, 1), &sl2_elem_get(a, 1, 1), a01a11);
+  _mm256_storeu2_m128i(&sl2_elem_get(a, 0, 0), &sl2_elem_get(a, 1, 0), a00a10);
 }
 #else
 static inline
 void sl2_mul_buf_right(sl2_t a, unsigned char *buf, size_t n) {
-  gf2p127_t a00 = a[0][0];
-  gf2p127_t a01 = a[0][1];
-  gf2p127_t a10 = a[1][0];
-  gf2p127_t a11 = a[1][1];
+  gf2p127_t a00 = sl2_elem_get(a, 0, 0);
+  gf2p127_t a01 = sl2_elem_get(a, 0, 1);
+  gf2p127_t a10 = sl2_elem_get(a, 1, 0);
+  gf2p127_t a11 = sl2_elem_get(a, 1, 1);
   size_t i;
   for (i = 0; i < n; i++) {
     sl2_mul_bits_right(&a00, &a01, &a10, &a11, buf[i]);
   }
-  a[0][0] = a00;
-  a[0][1] = a01;
-  a[1][0] = a10;
-  a[1][1] = a11;
+  sl2_elem_set(a, 0, 0, a00);
+  sl2_elem_set(a, 0, 1, a01);
+  sl2_elem_set(a, 1, 0, a10);
+  sl2_elem_set(a, 1, 1, a11);
 }
 #endif
 
@@ -223,62 +225,62 @@ static inline
 void sl2_mul(sl2_t c, sl2_t a, sl2_t b) {
   // Strassen algorithm
   gf2p127_t m0, m1, m2, m3, m4, m5, m6;
-  m0 = gf2p127_mul(gf2p127_add(a[0][0], a[1][1]),
-                   gf2p127_add(b[0][0], b[1][1]));
-  m1 = gf2p127_mul(gf2p127_add(a[1][0], a[1][1]), b[0][0]);
-  m2 = gf2p127_mul(a[0][0], gf2p127_add(b[0][1], b[1][1]));
-  m3 = gf2p127_mul(a[1][1], gf2p127_add(b[1][0], b[0][0]));
-  m4 = gf2p127_mul(gf2p127_add(a[0][0], a[0][1]), b[1][1]);
-  m5 = gf2p127_mul(gf2p127_add(a[1][0], a[0][0]),
-                   gf2p127_add(b[0][0], b[0][1]));
-  m6 = gf2p127_mul(gf2p127_add(a[0][1], a[1][1]),
-                   gf2p127_add(b[1][0], b[1][1]));
-  c[0][0] = gf2p127_add(gf2p127_add(m0, m3), gf2p127_add(m4, m6));
-  c[0][1] = gf2p127_add(m2, m4);
-  c[1][0] = gf2p127_add(m1, m3);
-  c[1][1] = gf2p127_add(gf2p127_add(m0, m1), gf2p127_add(m2, m5));
+  m0 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 0, 0), sl2_elem_get(a, 1, 1)),
+                   gf2p127_add(sl2_elem_get(b, 0, 0), sl2_elem_get(b, 1, 1)));
+  m1 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 1, 0), sl2_elem_get(a, 1, 1)), sl2_elem_get(b, 0, 0));
+  m2 = gf2p127_mul(sl2_elem_get(a, 0, 0), gf2p127_add(sl2_elem_get(b, 0, 1), sl2_elem_get(b, 1, 1)));
+  m3 = gf2p127_mul(sl2_elem_get(a, 1, 1), gf2p127_add(sl2_elem_get(b, 1, 0), sl2_elem_get(b, 0, 0)));
+  m4 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 0, 0), sl2_elem_get(a, 0, 1)), sl2_elem_get(b, 1, 1));
+  m5 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 1, 0), sl2_elem_get(a, 0, 0)),
+                   gf2p127_add(sl2_elem_get(b, 0, 0), sl2_elem_get(b, 0, 1)));
+  m6 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 0, 1), sl2_elem_get(a, 1, 1)),
+                   gf2p127_add(sl2_elem_get(b, 1, 0), sl2_elem_get(b, 1, 1)));
+  sl2_elem_set(c, 0, 0, gf2p127_add(gf2p127_add(m0, m3), gf2p127_add(m4, m6)));
+  sl2_elem_set(c, 0, 1, gf2p127_add(m2, m4));
+  sl2_elem_set(c, 1, 0, gf2p127_add(m1, m3));
+  sl2_elem_set(c, 1, 1, gf2p127_add(gf2p127_add(m0, m1), gf2p127_add(m2, m5)));
 }
 
 static inline
 void sl2_mul_optimized(sl2_t c, sl2_t a, sl2_t b) {
   gf2p127_t t0, t1, t2, t3, t4, t5;
-  t0 = gf2p127_add(a[1][0], a[1][1]);
-  t1 = gf2p127_add(a[1][1], a[0][1]);
-  t2 = gf2p127_add(a[1][1], a[0][0]);
-  t3 = gf2p127_add(b[1][1], b[0][0]);
-  t4 = gf2p127_add(b[1][0], b[1][1]);
-  t5 = gf2p127_add(b[1][1], b[0][1]);
+  t0 = gf2p127_add(sl2_elem_get(a, 1, 0), sl2_elem_get(a, 1, 1));
+  t1 = gf2p127_add(sl2_elem_get(a, 1, 1), sl2_elem_get(a, 0, 1));
+  t2 = gf2p127_add(sl2_elem_get(a, 1, 1), sl2_elem_get(a, 0, 0));
+  t3 = gf2p127_add(sl2_elem_get(b, 1, 1), sl2_elem_get(b, 0, 0));
+  t4 = gf2p127_add(sl2_elem_get(b, 1, 0), sl2_elem_get(b, 1, 1));
+  t5 = gf2p127_add(sl2_elem_get(b, 1, 1), sl2_elem_get(b, 0, 1));
   
   gf2p127_t M0, M1, M2, M3, M4, M5, M6;
-  M0 = gf2p127_mul(a[0][0], b[0][0]);
-  M1 = gf2p127_mul(a[0][1], b[1][0]);
-  M2 = gf2p127_mul(a[1][0], t3);
-  M3 = gf2p127_mul(a[1][1], b[1][1]);
+  M0 = gf2p127_mul(sl2_elem_get(a, 0, 0), sl2_elem_get(b, 0, 0));
+  M1 = gf2p127_mul(sl2_elem_get(a, 0, 1), sl2_elem_get(b, 1, 0));
+  M2 = gf2p127_mul(sl2_elem_get(a, 1, 0), t3);
+  M3 = gf2p127_mul(sl2_elem_get(a, 1, 1), sl2_elem_get(b, 1, 1));
   M4 = gf2p127_mul(t0, t4);
   M5 = gf2p127_mul(t1, t5);
-  M6 = gf2p127_mul(t2, b[0][1]);
+  M6 = gf2p127_mul(t2, sl2_elem_get(b, 0, 1));
 
-  c[0][0] = gf2p127_add(M0, M1);
-  c[0][1] = gf2p127_add(M4, M6);
-  c[1][0] = gf2p127_add(M2, M5);
-  c[1][1] = gf2p127_add(gf2p127_add(M4, M5), gf2p127_add(M1, M3));
+  sl2_elem_set(c, 0, 0, gf2p127_add(M0, M1));
+  sl2_elem_set(c, 0, 1, gf2p127_add(M4, M6));
+  sl2_elem_set(c, 1, 0, gf2p127_add(M2, M5));
+  sl2_elem_set(c, 1, 1, gf2p127_add(gf2p127_add(M4, M5), gf2p127_add(M1, M3)));
 
-  // gf2p127_hex(new_c00, c[0][0]);
-  // gf2p127_hex(new_c01, c[0][1]);
-  // gf2p127_hex(new_c10, c[1][0]);
-  // gf2p127_hex(new_c11, c[1][1]);
+  // gf2p127_hex(new_c00, sl2_elem_get(c, 0, 0));
+  // gf2p127_hex(new_c01, sl2_elem_get(c, 0, 1));
+  // gf2p127_hex(new_c10, sl2_elem_get(c, 1, 0));
+  // gf2p127_hex(new_c11, sl2_elem_get(c, 1, 1));
 
   // gf2p127_t m0, m1, m2, m3, m4, m5, m6;
-  // m0 = gf2p127_mul(gf2p127_add(a[0][0], a[1][1]),
-  //                  gf2p127_add(b[0][0], b[1][1]));
-  // m1 = gf2p127_mul(gf2p127_add(a[1][0], a[1][1]), b[0][0]);
-  // m2 = gf2p127_mul(a[0][0], gf2p127_add(b[0][1], b[1][1]));
-  // m3 = gf2p127_mul(a[1][1], gf2p127_add(b[1][0], b[0][0]));
-  // m4 = gf2p127_mul(gf2p127_add(a[0][0], a[0][1]), b[1][1]);
-  // m5 = gf2p127_mul(gf2p127_add(a[1][0], a[0][0]),
-  //                  gf2p127_add(b[0][0], b[0][1]));
-  // m6 = gf2p127_mul(gf2p127_add(a[0][1], a[1][1]),
-  //                  gf2p127_add(b[1][0], b[1][1]));
+  // m0 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 0, 0), sl2_elem_get(a, 1, 1)),
+  //                  gf2p127_add(sl2_elem_get(b, 0, 0), sl2_elem_get(b, 1, 1)));
+  // m1 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 1, 0), sl2_elem_get(a, 1, 1)), sl2_elem_get(b, 0, 0));
+  // m2 = gf2p127_mul(sl2_elem_get(a, 0, 0), gf2p127_add(sl2_elem_get(b, 0, 1), sl2_elem_get(b, 1, 1)));
+  // m3 = gf2p127_mul(sl2_elem_get(a, 1, 1), gf2p127_add(sl2_elem_get(b, 1, 0), sl2_elem_get(b, 0, 0)));
+  // m4 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 0, 0), sl2_elem_get(a, 0, 1)), sl2_elem_get(b, 1, 1));
+  // m5 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 1, 0), sl2_elem_get(a, 0, 0)),
+  //                  gf2p127_add(sl2_elem_get(b, 0, 0), sl2_elem_get(b, 0, 1)));
+  // m6 = gf2p127_mul(gf2p127_add(sl2_elem_get(a, 0, 1), sl2_elem_get(a, 1, 1)),
+  //                  gf2p127_add(sl2_elem_get(b, 1, 0), sl2_elem_get(b, 1, 1)));
 
   // gf2p127_hex(old_c00, gf2p127_add(gf2p127_add(m0, m3), gf2p127_add(m4, m6)));
   // gf2p127_hex(old_c01, gf2p127_add(m2, m4));
@@ -295,10 +297,10 @@ void sl2_mul_optimized(sl2_t c, sl2_t a, sl2_t b) {
   //   printf("old_c11: %s, new_c11: %s\n", old_c11, new_c11);
   // printf("====================================\n");
 
-  // c[0][0] = gf2p127_add(gf2p127_add(m0, m3), gf2p127_add(m4, m6));
-  // c[0][1] = gf2p127_add(m2, m4);
-  // c[1][0] = gf2p127_add(m1, m3);
-  // c[1][1] = gf2p127_add(gf2p127_add(m0, m1), gf2p127_add(m2, m5));
+  // sl2_elem_get(c, 0, 0) = gf2p127_add(gf2p127_add(m0, m3), gf2p127_add(m4, m6));
+  // sl2_elem_get(c, 0, 1) = gf2p127_add(m2, m4);
+  // sl2_elem_get(c, 1, 0) = gf2p127_add(m1, m3);
+  // sl2_elem_get(c, 1, 1) = gf2p127_add(gf2p127_add(m0, m1), gf2p127_add(m2, m5));
 }
 
 
@@ -314,30 +316,30 @@ void sl2_mul_byte_right(sl2_t a, unsigned char byte, sl2_t m[256]) {
 
 static inline
 void sl2_init(sl2_t m[2]) {
-  m[0][0][0] = gf2p127_from_int(2);
-  m[0][0][1] = gf2p127_from_int(1);
-  m[0][1][0] = gf2p127_from_int(1);
-  m[0][1][1] = gf2p127_from_int(0);
-  m[1][0][0] = gf2p127_from_int(2);
-  m[1][0][1] = gf2p127_from_int(3);
-  m[1][1][0] = gf2p127_from_int(1);
-  m[1][1][1] = gf2p127_from_int(1);
+  sl2_elem_set(m[0], 0, 0, gf2p127_from_int(2));
+  sl2_elem_set(m[0], 0, 1, gf2p127_from_int(1));
+  sl2_elem_set(m[0], 1, 0, gf2p127_from_int(1));
+  sl2_elem_set(m[0], 1, 1, gf2p127_from_int(0));
+  sl2_elem_set(m[1], 0, 0, gf2p127_from_int(2));
+  sl2_elem_set(m[1], 0, 1, gf2p127_from_int(3));
+  sl2_elem_set(m[1], 1, 0, gf2p127_from_int(1));
+  sl2_elem_set(m[1], 1, 1, gf2p127_from_int(1));
 }
 
 static inline
 void sl2_unit(sl2_t a) {
-  a[0][0] = gf2p127_from_int(1);
-  a[0][1] = gf2p127_from_int(0);
-  a[1][0] = gf2p127_from_int(0);
-  a[1][1] = gf2p127_from_int(1);
+  sl2_elem_set(a, 0, 0, gf2p127_from_int(1));
+  sl2_elem_set(a, 0, 1, gf2p127_from_int(0));
+  sl2_elem_set(a, 1, 0, gf2p127_from_int(0));
+  sl2_elem_set(a, 1, 1, gf2p127_from_int(1));
 }
 
 static inline
 char *sl2_hex(char *buf, sl2_t a) {
-  gf2p127_hex(&buf[0],  a[0][0]);
-  gf2p127_hex(&buf[32], a[0][1]);
-  gf2p127_hex(&buf[64], a[1][0]);
-  gf2p127_hex(&buf[96], a[1][1]);
+  gf2p127_hex(&buf[0],  sl2_elem_get(a, 0, 0));
+  gf2p127_hex(&buf[32], sl2_elem_get(a, 0, 1));
+  gf2p127_hex(&buf[64], sl2_elem_get(a, 1, 0));
+  gf2p127_hex(&buf[96], sl2_elem_get(a, 1, 1));
   return buf;
 }
 
