bb82d84c91f2047d10c458d84f2cec5f33be0d93
waterlens <waterlens@waterlens.moe>
2025-02-06 20:16:26 +0800

    remove unused tests

--- tests/main.c
+++ tests/main.c
@@ -2,7 +2,6 @@
 
 extern SUITE(gf2p127);
 extern SUITE(sl2);
-extern SUITE(sl2o);
 
 GREATEST_MAIN_DEFS();
 
@@ -10,6 +9,5 @@ int main(int argc, char **argv) {
   GREATEST_MAIN_BEGIN();
   RUN_SUITE(gf2p127);
   RUN_SUITE(sl2);
-  RUN_SUITE(sl2o);
   GREATEST_MAIN_END();
 }
